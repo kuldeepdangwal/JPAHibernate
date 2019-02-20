4797df1a04191d2b949fdc6efe806e9860312609
kuldeepdangwal <47214891+kuldeepdangwal@users.noreply.github.com>
2019-02-20 15:05:18 +0530

    Add files via upload

---
+++ OneToOneBiDirectionMapping/src/com/cg/project/client/MainClass.java
@@ -0,0 +1,11 @@
+package com.cg.project.client;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("JPA-PU");
+	}
+}
